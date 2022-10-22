8416513907b9f292829c0f3e04baf1925b23fdb2
Barqah <71264201+Barqah-Xiex@users.noreply.github.com>
2022-10-22 16:37:38 +0700

    Create bot.sh

---
+++ bot.sh
@@ -0,0 +1,24 @@
+echo "masukan nomor Owner menggunakan kode negara"
+echo "contoh: 628979059392"
+read nomor
+echo ""
+echo "masukan passwordmu"
+read pass
+echo ""
+echo "masukan idbot"
+read id
+echo ""
+echo ""
+echo "masuka perintah"
+echo "[+] start"
+echo "[+] restart"
+echo "[+] stop"
+echo "[+] qr"
+
+
+while [ true ]
+do
+    read command
+    content=$(wget http://xiex.my.id/aksesbot/$nomor/$pass/$command/$id -q -O -)
+    echo $content
+done
